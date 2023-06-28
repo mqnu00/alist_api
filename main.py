92e266fd2e4b694f91196e881ccf575dbe9238d5
mqnu00 <647983952@qq.com>
2023-06-28 14:09:04 +0800

    新增断点续传功能，但是怎么看怎么简陋，话说都加了这个感觉也可以加个多线程下载了。

--- main.py
+++ main.py
@@ -43,45 +43,69 @@ def download(host, base_path, path, cnt):
     with open("res" + str(cnt) + ".txt", encoding='utf-8') as f:
         name_list = json.loads(f.read())
 
+    # 获取下载文件的信息
     count = 1
     for info in name_list["data"]["content"]:
         name = info['name']
         name_type = info["type"]
         print(name)
         print(str(count) + '/' + str(len(name_list["data"]["content"])))
+        # 下载的文件存放的路径
+        filepath = base_path + '/' + path.split('/')[-1] + '/' + name
         count = count + 1
+        # 文件夹判断
         if name_type == 1:
             print("这是一个文件夹")
             get_list(host, path + '/' + name, cnt + 1)
             download(host, base_path + '/' + path.split('/')[-1], path + '/' + name, cnt + 1)
             continue
-        if os.path.exists(base_path + '/' + path.split('/')[-1] + '/' + name):
-            print("已存在")
-            continue
+        # 获取文件的下载url
         download_url = get_download_url(host, path + '/' + name)
-        try :
+        try:
             response = requests.get(download_url, stream=True)
         except Exception:
             print("不知道出了什么问题")
             continue
+        # 获取文件大小
         total_size_in_bytes = int(response.headers.get('content-length', 0))
+        # 每次下载的字长
         block_size = 1024
 
-        progress_bar = tqdm(total=total_size_in_bytes, unit='iB', unit_scale=True)
-
-        with open(base_path + '/' + path.split('/')[-1] + '/' + name, 'wb') as file:
+        # 获取以前下载的大小，断点续传
+        if os.path.exists(filepath):
+            first_byte = os.path.getsize(filepath)
+        else:
+            first_byte = 0
+        if first_byte >= total_size_in_bytes:
+            print("已下载完成")
+            continue
+        header = headers
+        header['Range'] = f"bytes={first_byte}-{total_size_in_bytes}"
+        response = requests.get(download_url, stream=True, headers=header)
+        # 显示下载进度条
+        progress_bar = tqdm(
+            total=total_size_in_bytes,
+            initial=first_byte,
+            unit='iB',
+            unit_scale=True,
+            desc=filepath
+        )
+        # 数据下载到文件
+        with open(filepath, 'wb') as file:
             for data in response.iter_content(block_size):
-                progress_bar.update(len(data))
-                file.write(data)
+                if data:
+                    progress_bar.update(len(data))
+                    file.write(data)
 
         progress_bar.close()
 
 
 if __name__ == "__main__":
-    url = "https://www.asmrgay.com/asmr/%E4%B8%AD%E6%96%87%E9%9F%B3%E5%A3%B0/%E5%96%B5%E8%A5%BF%E6%97%A9%E7%82%B9%E7%9D%A1"
+    url = "网址"
     parseresult = parse.urlparse(url)
     scheme = parseresult.scheme
     netloc = parseresult.netloc
     path = parse.unquote(parseresult.path)
     host = f"{scheme}://{netloc}"
-    download(host, "f:/", path, 0)
+    base_path = "下载的文件存放的地址"
+    download(host, base_path, path, 0)
